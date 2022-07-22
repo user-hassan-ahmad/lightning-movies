6e7782ad98bc0f37332add514ff9ffd60b4fc5e9
Hassan Ahmad <hassan.ahmad.ucl@gmail.com>
2022-07-22 19:38:42 +0100

    config changes

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from "vite";
-import mix from "vite-plugin-mix";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import mix from 'vite-plugin-mix';
+import vue from '@vitejs/plugin-vue';
 // const port = process.env.PORT || 'http://localhost:8080';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // proxy: {
-    // 	'/api': {
-    // 		target: port,
-    // 		changeOrigin: true,
-    // 		secure: false,
-    // 		rewrite: (path) => path.replace(/^\/api/, ''),
-    // 	},
-    // },
-  },
-  plugins: [
-    vue(),
-    mix({
-      handler: "./server.js",
-    }),
-  ],
+	server: {
+		// proxy: {
+		// 	'/api': {
+		// 		target: port,
+		// 		changeOrigin: true,
+		// 		secure: false,
+		// 		rewrite: (path) => path.replace(/^\/api/, ''),
+		// 	},
+		// },
+	},
+	plugins: [
+		vue(),
+		mix({
+			handler: './server.js',
+			adapter: vercelAdapter(),
+		}),
+	],
 });
